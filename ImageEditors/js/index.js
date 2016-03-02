50543e33d2cbe23ac6979eecd38373ee40330cb4
yyyama <yuuya-yamada@aeroasahi.co.jp>
2016-03-02 18:31:25 +0900

    index.htmlの更新

---
+++ ImageEditors/js/index.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="D:\Git_WorkSpace\ImageEditors\ImageEditors\lib/jquery/jquery-2.2.1.js" />
+/// <reference path="D:\Git_WorkSpace\ImageEditors\ImageEditors\lib/bootstrap/bootstrap.js" />
+
+$(function () {
+    $("#left_contents").click(function (e) {
+        alert("左コンテンツ");
+    });
+    $("#main_contents").click(function (e) {
+        alert("メインコンテンツ");
+    });
+    $("#right_contents").click(function (e) {
+        alert("右コンテンツ");
+    });
+})
